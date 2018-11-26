4f4a772cceb09348c89d2bef03de5ca4988de87f
eureka84 <angelosciarra@ymail.com>
2018-11-26 17:01:25 +0100

    improved Hamming

--- hamming/src/main/kotlin/Hamming.kt
+++ hamming/src/main/kotlin/Hamming.kt
@@ -1,10 +1,8 @@
 object Hamming {
 
-    fun compute(a: String, b: String): Int {
-        if (a.length != b.length) {
-            throw IllegalArgumentException("left and right strands must be of equal length.")
-        }
-        return a.zip(b).count { (a, b) -> a != b }
+    fun compute(left: String, right: String): Int {
+        require(left.length == right.length) { "left and right strands must be of equal length." }
+        return left.zip(right).count { (leftChar, rightChar) -> leftChar != rightChar }
     }
 
 }
