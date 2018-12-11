42b067b6efc2a8fafd4182471eeeb24009226eb5
eureka84 <angelosciarra@ymail.com>
2018-12-11 20:11:43 +0100

    minor: reformat

--- anagram/src/main/kotlin/Anagram.kt
+++ anagram/src/main/kotlin/Anagram.kt
@@ -1,14 +1,14 @@
-class Anagram(private val value: String){
+class Anagram(private val value: String) {
 
-    private val letters= sortedLetters(value)
+    private val letters = sortedLetters(value)
 
     private fun sortedLetters(input: String) = input.toLowerCase().toCharArray().sorted()
 
     private fun sameWord(s: String) = s.toLowerCase() == value.toLowerCase()
 
     fun match(inputs: List<String>): Set<String> =
-        inputs.filter { s -> !sameWord(s) }
-              .filter { s -> sortedLetters(s) == letters }
-              .toSet()
+            inputs.filter { s -> !sameWord(s) }
+                  .filter { s -> sortedLetters(s) == letters }
+                  .toSet()
 
 }
