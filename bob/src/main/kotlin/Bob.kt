2121f6ed7ef1a8c2a203ce532c9351f15c8ac708
eureka84 <angelosciarra@ymail.com>
2018-12-16 09:38:43 +0100

    implemented Bob

--- bob/src/main/kotlin/Bob.kt
+++ bob/src/main/kotlin/Bob.kt
@@ -1,3 +1,24 @@
 object Bob {
-    fun hey(greeting: String): String = TODO()
+    fun hey(greeting: String): String =
+            when {
+                greeting.isSilence() -> "Fine. Be that way!"
+                greeting.isShouted() -> "Whoa, chill out!"
+                greeting.isAQuestion() -> "Sure."
+                else -> "Whatever."
+            }
 }
+
+private fun String.isSilence(): Boolean {
+    val spaces = Regex("[\\s\\t\\n]")
+    return this.replace(spaces, "").isEmpty()
+}
+
+private fun String.isAQuestion(): Boolean = this.trim().endsWith("?")
+
+private fun String.isShouted(): Boolean {
+    val nonLetters = Regex("[^A-Za-z]")
+    val letters = this.replace(nonLetters, "")
+    return !letters.isEmpty() && letters.isUpperCase()
+}
+
+private fun String.isUpperCase(): Boolean = this.all { c -> c in ('A'..'Z') }
