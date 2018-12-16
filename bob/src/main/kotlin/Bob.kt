24db47db3a31e122cda0a65c1cf113c12d520de6
eureka84 <angelosciarra@ymail.com>
2018-12-16 11:38:26 +0100

    refactored: simplified factoring out trim using a let block and using method reference

--- bob/src/main/kotlin/Bob.kt
+++ bob/src/main/kotlin/Bob.kt
@@ -1,20 +1,22 @@
 object Bob {
     fun hey(greeting: String): String =
-            when {
-                greeting.isSilence() -> "Fine. Be that way!"
-                greeting.isShouted() -> "Whoa, chill out!"
-                greeting.isAQuestion() -> "Sure."
-                else -> "Whatever."
+            greeting.trim().let { phrase ->
+                when {
+                    phrase.isSilence() -> "Fine. Be that way!"
+                    phrase.isShouted() -> "Whoa, chill out!"
+                    phrase.isAQuestion() -> "Sure."
+                    else -> "Whatever."
+                }
             }
 }
 
-private fun String.isSilence(): Boolean = this.trim().isEmpty()
+private fun String.isSilence(): Boolean = isEmpty()
 
-private fun String.isAQuestion(): Boolean = this.trim().endsWith("?")
+private fun String.isAQuestion(): Boolean = endsWith("?")
 
 private fun String.isShouted(): Boolean {
-    val onlyLetters = this.filter { c -> c.isLetter() }
+    val onlyLetters = this.filter(Char::isLetter)
     return !onlyLetters.isEmpty() && onlyLetters.isUpperCase()
 }
 
-private fun String.isUpperCase(): Boolean = this.all { c -> c.isUpperCase() }
+private fun String.isUpperCase(): Boolean = this.all(Char::isUpperCase)
