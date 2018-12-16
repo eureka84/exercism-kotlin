28ece2de3b4bc416921b7ba38d39c68f162bbe07
eureka84 <angelosciarra@ymail.com>
2018-12-16 21:43:20 +0100

    improved beer song

--- beer-song/src/main/kotlin/BeerSong.kt
+++ beer-song/src/main/kotlin/BeerSong.kt
@@ -5,22 +5,21 @@ object BeerSong {
     private fun verse(bottlesOnTheWall: Int) =
             firstVerse(bottlesOnTheWall) + secondVerse(bottlesOnTheWall, bottlesOnTheWall - 1)
 
-    private fun firstVerse(n: Int) =
-            "${n.bottles().capitalize()} of beer on the wall, ${n.bottles()} of beer.\n"
+    private fun firstVerse(n: Int) = "${n.onTheWall().capitalize()}, ${n.bottles()}.\n"
 
     private fun secondVerse(current: Int, remaining: Int): String =
             when (current) {
-                0 -> "Go to the store and buy some more, ${99.onTheWall()}"
-                1 -> "Take it down and pass it around, ${remaining.onTheWall()}"
-                else -> "Take one down and pass it around, ${remaining.onTheWall()}"
+                0 -> "Go to the store and buy some more, ${99.onTheWall()}.\n"
+                1 -> "Take it down and pass it around, ${remaining.onTheWall()}.\n"
+                else -> "Take one down and pass it around, ${remaining.onTheWall()}.\n"
             }
 
 
-    private fun Int.onTheWall(): String = "${this.bottles()} of beer on the wall.\n"
+    private fun Int.onTheWall(): String = "${this.bottles()} on the wall"
 
     private fun Int.bottles(): String = when (this) {
-        1 -> "1 bottle"
-        0 -> "no more bottles"
-        else -> "$this bottles"
+        1 -> "1 bottle of beer"
+        0 -> "no more bottles of beer"
+        else -> "$this bottles of beer"
     }
 }
