e7639ce634551b7dbcaaaf69330d79623acf578d
eureka84 <angelosciarra@ymail.com>
2018-12-17 01:20:16 +0100

    improved beer song

--- beer-song/src/main/kotlin/BeerSong.kt
+++ beer-song/src/main/kotlin/BeerSong.kt
@@ -1,22 +1,23 @@
 object BeerSong {
-    fun verses(from: Int, to: Int): String =
-            (from downTo to).joinToString(separator = "\n") { bottlesOnTheWall -> verse(bottlesOnTheWall) }
 
-    private fun verse(n: Int) = firstVerse(n) + secondVerse(n)
+    fun verses(from: Int, to: Int) = (from downTo to).joinToString(separator = "\n") { bottlesOnTheWall ->
+        verse(bottlesOnTheWall)
+    }
 
-    private fun firstVerse(n: Int) = "${n.onTheWall().capitalize()}, ${n.bottles()}.\n"
+    private fun verse(n: Int) = firstLine(n) + secondLine(n)
 
-    private fun secondVerse(n: Int): String =
-            when (n) {
-                0 -> "Go to the store and buy some more, ${99.onTheWall()}.\n"
-                1 -> "Take it down and pass it around, ${(n - 1).onTheWall()}.\n"
-                else -> "Take one down and pass it around, ${(n -1).onTheWall()}.\n"
-            }
+    private fun firstLine(n: Int) = "${n.onTheWall().capitalize()}, ${n.bottles()}.\n"
+
+    private fun secondLine(n: Int) = when (n) {
+        0 -> "Go to the store and buy some more, ${99.onTheWall()}.\n"
+        1 -> "Take it down and pass it around, ${(n - 1).onTheWall()}.\n"
+        else -> "Take one down and pass it around, ${(n - 1).onTheWall()}.\n"
+    }
 
 
-    private fun Int.onTheWall(): String = "${this.bottles()} on the wall"
+    private fun Int.onTheWall() = "${this.bottles()} on the wall"
 
-    private fun Int.bottles(): String = when (this) {
+    private fun Int.bottles() = when (this) {
         1 -> "1 bottle of beer"
         0 -> "no more bottles of beer"
         else -> "$this bottles of beer"
